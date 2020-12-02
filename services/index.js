008f4f93218ee4b363c37a5efbb1217a9dac763e
Daniil Borikin <borikin.daniil@gmail.com>
2020-12-02 15:10:22 +0200

    Add news services to services

--- services/index.js
+++ services/index.js
@@ -1,7 +1,9 @@
 import AdsService from './ads';
+import NewsService from './news';
 
 export default (context) => {
   return {
     ads: new AdsService(context),
+    news: new NewsService(context),
   };
 };
