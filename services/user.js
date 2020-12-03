90d4032da9eaa630d4376313daf43e8639e40e24
BorikinDaniil <borikin.daniil@gmail.com>
2020-12-03 14:40:52 +0200

    Add submitContactForm method to user services

--- services/user.js
+++ services/user.js
@@ -4,4 +4,7 @@ export default class UserService extends ApiService {
   getContactForm() {
     return this.axios.$get(`/contact`);
   }
+  submitContactForm(data) {
+    return this.axios.$post(`/contact`, data);
+  }
 }
