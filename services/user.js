d8299e687dec5da5e9eb2395fd170964ff6abdb6
BorikinDaniil <borikin.daniil@gmail.com>
2020-12-09 09:59:04 +0200

    Update methods in user services

--- services/user.js
+++ services/user.js
@@ -7,4 +7,28 @@ export default class UserService extends ApiService {
   submitContactForm(data) {
     return this.axios.$post(`/contact`, data);
   }
+  getUserData(token) {
+    return this.axios.$get(`/auth/client/personal`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
+  saveUserData(data, token) {
+    return this.axios.$put(`/auth/client/change`, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
+  ChangeUserPassword(data, token) {
+    return this.axios.$put(`/auth/client/changePassword`, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
+  getCities(params) {
+    return this.axios.$get(`/filters/cities`, { params });
+  }
 }
